39d408a8d4052ab225cef97b2df7c519721aa93c
Brent Goss <brgoss@suddenlink.net>
2016-04-04 05:01:47 -0500

    Switches to Paul's grader

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
 
 	def self.testing_code
 		#return true
-		return %x{python3 app/models/grader.py app/models/testcode.txt "'hELLO_worLD'" }
+		return %x{python3 Paul/graderPaul.py app/models/testcode.txt "'hELLO_worLD'" }
 	end
 end
